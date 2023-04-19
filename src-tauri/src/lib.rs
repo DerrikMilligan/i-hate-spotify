286091192d7784c4f02c64123f42450f42aa5ae3
Derrik Milligan <derrik.milligan.dev@gmail.com>
2023-04-19 00:13:02 -0600

    Some initial messing around with db stuff in rust and the frontend.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,25 @@
+use std::sync::RwLock;
+
 use reqwest;
+use sqlite::Connection;
+use sqlite::State;
+
+pub struct InnerAppState {
+    pub db: Connection,
+}
+
+// Needed to be able to aquite the locks for SQLite
+unsafe impl Send for InnerAppState {}
+unsafe impl Sync for InnerAppState {}
+
+// impl InnerAppState {
+//     pub fn reset(&mut self) {
+//         // do stuff
+//         sqlite::open(":memory:").unwrap();
+//     }
+// }
+
+pub struct AppState(pub RwLock<InnerAppState>);
 
 // A custom error type that represents all possible in our command
 #[derive(Debug, thiserror::Error)]
@@ -9,24 +30,68 @@ enum Error {
 
 // we must also implement serde::Serialize
 impl serde::Serialize for Error {
-  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-  where
-    S: serde::ser::Serializer,
-  {
-    serializer.serialize_str(self.to_string().as_ref())
-  }
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::ser::Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_ref())
+    }
 }
 
 #[tauri::command]
 async fn hello_command(name: String) -> Result<String, Error> {
-  let body = reqwest::get("https://randomuser.me/api/").await?.text().await?;
-  Ok(body.into())
+    let body = reqwest::get("https://randomuser.me/api/")
+        .await?
+        .text()
+        .await?;
+
+    Ok(body.into())
+}
+
+#[tauri::command]
+async fn create_user(name: String, state: tauri::State<'_, AppState>) -> Result<String, Error> {
+    let state= state.0.write().unwrap();
+
+    let _ = state.db.execute("
+        CREATE TABLE IF NOT EXISTS Users (
+            id INTPRIMARY KEY,
+            name VARCHAR(255)
+        );
+    ");
+
+    let _ = state.db.execute(format!("
+        INSERT INTO Users (name) VALUES 
+        (\"{name}\")
+    "));
+
+    Ok("success".to_string())
+}
+
+#[tauri::command]
+async fn get_users(state: tauri::State<'_, AppState>) -> Result<Vec<String>, Error> {
+    let state= state.0.write().unwrap();
+
+    let mut statement = state.db.prepare("SELECT * FROM Users;").unwrap();
+
+    let mut users : Vec<String> = Vec::new();
+
+    while let Ok(State::Row) = statement.next() {
+        let name = statement.read::<String, _>("name").unwrap();
+        users.push(name);
+    }
+
+    Ok(users)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![hello_command])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .manage(AppState(RwLock::new(InnerAppState{ db: sqlite::open(":memory:").unwrap() })))
+        .invoke_handler(tauri::generate_handler![
+            hello_command,
+            create_user,
+            get_users
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
